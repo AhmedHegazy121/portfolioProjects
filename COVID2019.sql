0fda9e17eff4163fb8d86b06564a904d07c4ef6e
AhmedHegazy121 <116463376+AhmedHegazy121@users.noreply.github.com>
2023-08-01 22:33:52 +0300

    Update COVID2019.sql

--- COVID2019.sql
+++ COVID2019.sql
@@ -4,23 +4,34 @@ order by 3 ,4
 select * 
 from PortiolioProject .. ConvidVaccinations
 order by 3 ,4
-
+ 
+-- Select Data that we are going to be starting with
+ 
  select  location, date  , total_cases , new_cases , total_deaths , population
  from PortiolioProject .. coviedDeaths
  order by 1,2
-
+ 
+-- to change the column from nvarchar to int
 
  alter table PortiolioProject .. coviedDeaths 
  alter column  total_deaths int 
  alter table PortiolioProject .. coviedDeaths 
  alter column  total_cases int 
 
-
+-- Total Cases vs Total Deaths
+ 
  select  location, date  , total_cases , population, (total_cases /population)*100 as  percentpopulationinfected
  from PortiolioProject .. coviedDeaths
  --where location like '%states%'
  order by 1,2
+ -- Total Cases vs Population
+ 
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+order by 1,2
 
+-- Countries with Highest Infection Rate compared to Population
 
  select  location, population, max (total_cases) as HighestinfectionCount ,  max((total_cases /population))*100 
  as percentpopulationinfected
@@ -29,14 +40,18 @@ order by 3 ,4
  where location is not null
  group by location, population
  order by  percentpopulationinfected desc
-
+ 
+-- Countries with Highest Death Count per Population
+ 
  select  location , max(cast (total_deaths as int))as TotalDeathCount
  from PortiolioProject .. coviedDeaths
  --where location like '%states%'
  where location is not null
  group by location
  order by TotalDeathCount  desc
-
+ 
+-- Showing contintents with the highest death count per population
+ 
  select  continent , max (cast (total_deaths as int))as TotalDeathCount
  from PortiolioProject .. coviedDeaths
  --where location like '%states%'
@@ -44,7 +59,7 @@ order by 3 ,4
  group by continent
  order by TotalDeathCount  desc
 
-
+-- GLOBAL NUMBERS
   
  Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, 
  SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
@@ -53,6 +68,8 @@ From PortiolioProject .. coviedDeaths
 where continent is not null 
 --Group By date
 order by 1,2
+
+ -- Total Population vs Vaccinations
 
  select dea.continent ,dea.location, dea.date, dea.population ,vac.new_vaccinations 
  ,sum(convert( float,vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as countvaccinations
